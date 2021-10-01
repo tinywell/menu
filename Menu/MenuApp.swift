fcb474286bf132f8cbd80c977f1bd4ab7757fe52
tinywell <tinywell@tinywell.com>
2021-10-01 20:40:06 +0800

    Initial Commit

---
+++ Menu/MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuApp.swift
+//  Menu
+//
+//  Created by tinywell on 2021/10/01.
+//
+
+import SwiftUI
+
+@main
+struct MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
