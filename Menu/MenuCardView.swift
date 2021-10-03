c6a2ba63d72b4adc00e28b4017f41ce386f5af0a
tinywell <tinywell@tinywell.com>
2021-10-03 15:09:19 +0800

    修复删除目标错误缺陷

--- Menu/MenuCardView.swift
+++ Menu/MenuCardView.swift
@@ -8,9 +8,13 @@
 import SwiftUI
 
 struct MenuCardView: View {
+    @EnvironmentObject var appData:AppData
+    
     @State var menu:MenuModel
     public let width:CGFloat=350
     public let height:CGFloat=263
+    
+    @State private var showConfirm=false
     
     var body: some View {
         ZStack{
@@ -39,8 +43,27 @@ struct MenuCardView: View {
         }.frame(width: width, height: height, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
         .clipShape(RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/))
         .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-        
-        
+        .onLongPressGesture {
+            self.showConfirm=true
+        }
+        .alert(isPresented: $showConfirm) {
+            Alert(
+                title: Text("此操作不可撤回"),
+                message: Text("确定删除这个项目吗？"),
+                primaryButton: .default(
+                    Text("取消"),
+                    action: {print("取消",menu.id,menu.name)}
+                ),
+                secondaryButton: .destructive(
+                    Text("确定"),
+                    action: {
+                        print("删除")
+                        self.appData.menus.removeAll { $0 == menu }
+                        Helper.saveMenus(menus: self.appData.menus)
+                    }
+                )
+            )
+        }    
     }
 }
 
--- Menu/MenuScrollView.swift
+++ Menu/MenuScrollView.swift
@@ -1,5 +1,5 @@
 //
-//  TestScrollView.swift
+//  MenuScrollView.swift
 //  Menu
 //
 //  Created by tinywell on 2021/10/02.
@@ -27,57 +27,23 @@ struct MenuScrollView: View {
         let currentOffset = CGFloat(currentIndex) * (menuWidth + spacing)-spacing
         
         GeometryReader(content: { geometry in
-            //        ScrollView(.horizontal, showsIndicators: false, content: {
             
             HStack(spacing:spacing){
                 ForEach(appData.menus){menu in
-                    //                ForEach(0..<appData.menus.count){_ in
+                    // ForEach(0..<appData.menus.count){_ in
                     HStack{
-                        MenuCardView(menu: menu)
-                            //                        MenuCardView(menu: appData.menus[$0])
-                            .onLongPressGesture {
-                                self.showConfirm=true
-                            }
-                            .alert(isPresented: $showConfirm) {
-                                Alert(
-                                    title: Text("此操作不可撤回"),
-                                    message: Text("确定删除这个项目吗？"),
-                                    primaryButton: .default(
-                                        Text("取消"),
-                                        action: {print("取消")}
-                                    ),
-                                    secondaryButton: .destructive(
-                                        Text("确定"),
-                                        action: {
-                                            print("删除")
-                                            self.appData.menus.removeAll { $0 == menu }
-                                            Helper.saveMenus(menus: self.appData.menus)
-                                        }
-                                    )
-                                )
-                            }
-                        
+                        MenuCardView(menu: menu).environmentObject(appData)
                     }
                     .offset(x: dragOffset - currentOffset)
                     .gesture(dragGesture)
-//                    .animation(isAnimation ?.spring():.none)
                     .animation(.spring())
-                    //                    .onChange(of: currentIndex, perform: { value in
-                    //                        isAnimation = true
-                    //                        /// 第一张的时候
-                    //                        if value == 0 {
-                    //                            isAnimation.toggle()
-                    //                            currentIndex = appData.menus.count - 2
-                    //                            /// 最后一张的时候currentIndex设置为1关闭动画
-                    //                        }else if value == appData.menus.count - 1 {
-                    //
-                    //                            isAnimation.toggle()
-                    //                            currentIndex = 1
-                    //                        }
-                    //                    })
+                    .onChange(of: currentIndex, perform: { value in
+                        currentIndex = max(min(currentIndex, appData.menus.count - 1), 0)
+                    })
                 }
             }
         })
+        
     }
 }
 
